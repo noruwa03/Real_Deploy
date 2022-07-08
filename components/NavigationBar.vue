118ed1a39a2f091d56c23499efea3c1eb18fe113
noruwa03 <noruwaobaseki@gmail.com>
2022-07-08 23:46:11 +0100

    Increased height of about-us card

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,45 +1,47 @@
 <template>
   <div>
-
     <div class="menu-card">
-      
-    </div>
- 
-    <nuxt-link class="logo-image"  to="/"> <img src="@/assets/images/logo.svg" alt="logo" /></nuxt-link>
+      <nuxt-link class="logo-image" to="/">
+        <img src="@/assets/images/logo.svg" alt="logo"
+      /></nuxt-link>
 
-    <div id="menu-btn" @click="menuHandler">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+      <div id="menu-btn" @click="menuHandler">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-   
 
     <!-- Navigation -->
     <nav>
       <div class="nav-item-container">
         <!-- Logo -->
-       
-        <nuxt-link class="navbar-brand logo-image" to="/"><img src="@/assets/images/logo.svg" alt="logo" /></nuxt-link>
+
+        <nuxt-link class="navbar-brand logo-image" to="/"
+          ><img src="@/assets/images/logo.svg" alt="logo"
+        /></nuxt-link>
         <!-- End of Logo -->
 
         <ul class="nav-item-list">
-          <li @click="removeSidebar" class="nav-item"><a class="nav-link" href="#about-us" >About</a></li>
-          
-          <li @click="removeSidebar"  class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+          <li @click="removeSidebar" class="nav-item">
+            <a class="nav-link" href="#about-us">About</a>
+          </li>
+
+          <li @click="removeSidebar" class="nav-item">
+            <a class="nav-link" href="#services">Services</a>
+          </li>
           <li class="nav-item"><a class="nav-link" href="">Portfolio</a></li>
           <li class="nav-item"><a class="nav-link" href="">Jobs</a></li>
           <li @click="removeSidebar" class="nav-item">
-          <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
-         
+            <nuxt-link class="nav-link" to="/contact">Contact</nuxt-link>
           </li>
         </ul>
 
         <div class="get-free-quote">
           <a class="anchor-link" href="#contact"> Get a free quote </a>
- 
         </div>
       </div>
     </nav>
@@ -50,8 +52,6 @@
 
 
 <script>
-
-
 export default {
   setup() {
     const menuHandler = () => {
@@ -62,12 +62,12 @@ export default {
       nav.classList.toggle("active");
       navItem.classList.toggle("active");
       navIcon2.classList.toggle("open");
-    }
+    };
 
     // const goto = () => {
     //   const el = document.querySelector("#contact");
     //   el.scrollIntoView(false);
-      
+
     // };
 
     const removeSidebar = () => {
@@ -78,19 +78,14 @@ export default {
       nav.classList.toggle("active");
       navItem.classList.toggle("active");
       navIcon2.classList.toggle("open");
-      
     };
 
-    
-
-    
     return {
-      menuHandler, removeSidebar, 
-    }
+      menuHandler,
+      removeSidebar,
+    };
   },
-
- 
-}
+};
 </script>
 
 <style>
